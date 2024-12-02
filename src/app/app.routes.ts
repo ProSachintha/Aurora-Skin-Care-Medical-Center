c87c6bc1d1dcf8e741c60e281c753249fb4624d9
sachintha <sachintha.s.gunawardhana@gmail.com>
2024-12-02 06:45:13 +0530

    Dashbord

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';   
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
 export const routes: Routes = [
     {path:"",component:AuthComponent},
     {path:"home",component:HomeComponent},
+    {path:"dashbord",component:DashbordComponent}
 ];
